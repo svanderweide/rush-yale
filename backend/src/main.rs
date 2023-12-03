da228e65174de22a31f812fc5cb3a9182218bc09
Simon Van Der Weide <jadenster12@gmail.com>
2023-12-03 16:26:29 -0500

    feat: implement basic actix-web server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer};
+
+#[get("/health-check")]
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(health_check))
+        .bind(("127.0.0.1", 8000))?
+        .run()
+        .await
 }
