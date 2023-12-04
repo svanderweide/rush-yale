df3145a63ff2de36fbab8fd4a60bce342bdad178
Simon Van Der Weide <jadenster12@gmail.com>
2023-12-03 19:20:08 -0500

    feat: implement TLS with rustls v0.21

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,10 +3,16 @@ use actix_web::{App, HttpServer};
 mod handlers;
 use handlers::health_check;
 
+mod tls;
+use tls::load_rustls_config;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // set up TLS
+    let rustls_config = load_rustls_config();
+
     HttpServer::new(|| App::new().service(health_check))
-        .bind(("127.0.0.1", 8000))?
+        .bind_rustls_021(("127.0.0.1", 8000), rustls_config)?
         .run()
         .await
 }
