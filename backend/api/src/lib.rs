7976cecbe9f0ea043fdf09970fb61a97a16880f3
Simon Van Der Weide <jadenster12@gmail.com>
2023-12-04 11:25:47 -0500

    feat: store database connection in app state

--- backend/api/src/lib.rs
+++ backend/api/src/lib.rs
@@ -1,4 +1,5 @@
 use actix_web::{App, HttpServer};
+use rush_yale_service::sea_orm::{Database, DatabaseConnection};
 
 mod handlers;
 use handlers::health_check;
@@ -6,13 +7,28 @@ use handlers::health_check;
 mod tls;
 use tls::load_rustls_config;
 
+#[derive(Debug, Clone)]
+struct AppState {
+    _conn: DatabaseConnection,
+}
+
 #[actix_web::main]
 async fn serve() -> std::io::Result<()> {
+    // load environment variables
+    dotenvy::dotenv().ok();
+    let db_url = std::env::var("DATABASE_URL").expect("'DATABASE_URL' should be set in .env file'");
+
+    // set up database connection
+    let _conn = Database::connect(&db_url).await.unwrap();
+
+    // create app state
+    let state = AppState { _conn };
+
     // set up TLS
     let rustls_config = load_rustls_config();
 
     // serve backend
-    HttpServer::new(|| App::new().service(health_check))
+    HttpServer::new(move || App::new().app_data(state.clone()).service(health_check))
         .bind_rustls_021(("127.0.0.1", 8000), rustls_config)?
         .run()
         .await
