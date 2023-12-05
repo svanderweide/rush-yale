52c9cf8f83279354a8f0b92dc4a79ca653b17c85
Simon Van Der Weide <jadenster12@gmail.com>
2023-12-05 01:24:37 -0500

    feat: add AppState extractor to route handlers

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -1,4 +1,9 @@
-use actix_web::{get, post, put, web::Path, HttpResponse};
+use crate::AppState;
+use actix_web::{
+    get, post, put,
+    web::{Data, Path},
+    HttpResponse,
+};
 
 #[get("/health-check")]
 async fn health_check() -> HttpResponse {
@@ -6,116 +11,160 @@ async fn health_check() -> HttpResponse {
 }
 
 #[get("/events")]
-async fn event_get_all_ids() -> HttpResponse {
+async fn event_get_all_ids(data: Data<AppState>) -> HttpResponse {
+    let _conn = &data.conn;
     todo!()
 }
 
 #[post("/events")]
-async fn event_create() -> HttpResponse {
+async fn event_create(data: Data<AppState>) -> HttpResponse {
+    let _conn = &data.conn;
     todo!()
 }
 
 #[get("/events/{id}")]
-async fn event_get(_id: Path<i32>) -> HttpResponse {
+async fn event_get(data: Data<AppState>, id: Path<i32>) -> HttpResponse {
+    let _conn = &data.conn;
+    let _id = id.into_inner();
     todo!()
 }
 
 #[put("/events/{id}")]
-async fn event_update(_id: Path<i32>) -> HttpResponse {
+async fn event_update(data: Data<AppState>, id: Path<i32>) -> HttpResponse {
+    let _conn = &data.conn;
+    let _id = id.into_inner();
     todo!()
 }
 
 #[get("/organizations")]
-async fn organization_get_all_ids() -> HttpResponse {
+async fn organization_get_all_ids(data: Data<AppState>) -> HttpResponse {
+    let _conn = &data.conn;
     todo!()
 }
 
 #[post("/organizations")]
-async fn organization_create() -> HttpResponse {
+async fn organization_create(data: Data<AppState>) -> HttpResponse {
+    let _conn = &data.conn;
     todo!()
 }
 
 #[get("/organizations/{id}")]
-async fn organization_get(_id: Path<i32>) -> HttpResponse {
+async fn organization_get(data: Data<AppState>, id: Path<i32>) -> HttpResponse {
+    let _conn = &data.conn;
+    let _id = id.into_inner();
     todo!()
 }
 
 #[put("/organizations/{id}")]
-async fn organization_update(_id: Path<i32>) -> HttpResponse {
+async fn organization_update(data: Data<AppState>, id: Path<i32>) -> HttpResponse {
+    let _conn = &data.conn;
+    let _id = id.into_inner();
     todo!()
 }
 
 #[get("/organizations/{id}/users")]
-async fn organization_get_all_users(_id: Path<i32>) -> HttpResponse {
+async fn organization_get_all_users(data: Data<AppState>, id: Path<i32>) -> HttpResponse {
+    let _conn = &data.conn;
+    let _id = id.into_inner();
     todo!()
 }
 
 #[get("/organization/{org_id}/users/{usr_id}")]
-async fn organization_get_user_status(_ids: Path<(i32, i32)>) -> HttpResponse {
+async fn organization_get_user_status(data: Data<AppState>, ids: Path<(i32, i32)>) -> HttpResponse {
+    let _conn = &data.conn;
+    let (_org_id, _usr_id) = ids.into_inner();
     todo!()
 }
 
 #[post("/organization/{org_id}/users/{usr_id}")]
-async fn organization_create_user_status(_ids: Path<(i32, i32)>) -> HttpResponse {
+async fn organization_create_user_status(
+    data: Data<AppState>,
+    ids: Path<(i32, i32)>,
+) -> HttpResponse {
+    let _conn = &data.conn;
+    let (_org_id, _usr_id) = ids.into_inner();
     todo!()
 }
 
 #[put("/organization/{org_id}/users/{usr_id}")]
-async fn organization_update_user_status(_ids: Path<(i32, i32)>) -> HttpResponse {
+async fn organization_update_user_status(
+    data: Data<AppState>,
+    ids: Path<(i32, i32)>,
+) -> HttpResponse {
+    let _conn = &data.conn;
+    let (_org_id, _usr_id) = ids.into_inner();
     todo!()
 }
 
 #[get("/threads")]
-async fn thread_get_all_ids() -> HttpResponse {
+async fn thread_get_all_ids(data: Data<AppState>) -> HttpResponse {
+    let _conn = &data.conn;
     todo!()
 }
 
 #[post("/threads")]
-async fn thread_create() -> HttpResponse {
+async fn thread_create(data: Data<AppState>) -> HttpResponse {
+    let _conn = &data.conn;
     todo!()
 }
 
 #[get("/threads/{id}")]
-async fn thread_get(_id: Path<i32>) -> HttpResponse {
+async fn thread_get(data: Data<AppState>, id: Path<i32>) -> HttpResponse {
+    let _conn = &data.conn;
+    let _id = id.into_inner();
     todo!()
 }
 
 #[post("/threads/{id}")]
-async fn thread_create_message(_id: Path<i32>) -> HttpResponse {
+async fn thread_create_message(data: Data<AppState>, id: Path<i32>) -> HttpResponse {
+    let _conn = &data.conn;
+    let _id = id.into_inner();
     todo!()
 }
 
 #[get("/users")]
-async fn user_get_all_ids() -> HttpResponse {
+async fn user_get_all_ids(data: Data<AppState>) -> HttpResponse {
+    let _conn = &data.conn;
     todo!()
 }
 
 #[post("/users")]
-async fn user_create() -> HttpResponse {
+async fn user_create(data: Data<AppState>) -> HttpResponse {
+    let _conn = &data.conn;
     todo!()
 }
 
 #[get("/users/{id}")]
-async fn user_get(_id: Path<i32>) -> HttpResponse {
+async fn user_get(data: Data<AppState>, id: Path<i32>) -> HttpResponse {
+    let _conn = &data.conn;
+    let _id = id.into_inner();
     todo!()
 }
 
 #[put("/users/{id}")]
-async fn user_update(_id: Path<i32>) -> HttpResponse {
+async fn user_update(data: Data<AppState>, id: Path<i32>) -> HttpResponse {
+    let _conn = &data.conn;
+    let _id = id.into_inner();
     todo!()
 }
 
 #[get("/users/{id}/events")]
-async fn user_get_events(_id: Path<i32>) -> HttpResponse {
+async fn user_get_events(data: Data<AppState>, id: Path<i32>) -> HttpResponse {
+    let _conn = &data.conn;
+    let _id = id.into_inner();
     todo!()
 }
 
 #[get("/users/{id}/statuses")]
-async fn user_get_statuses(_id: Path<i32>) -> HttpResponse {
+async fn user_get_statuses(data: Data<AppState>, id: Path<i32>) -> HttpResponse {
+    let _conn = &data.conn;
+    let _id = id.into_inner();
     todo!()
 }
 
 #[get("/users/{id}/threads")]
-async fn user_get_thread_ids(_id: Path<i32>) -> HttpResponse {
+async fn user_get_thread_ids(data: Data<AppState>, id: Path<i32>) -> HttpResponse {
+    let _conn = &data.conn;
+    let _id = id.into_inner();
     todo!()
 }
